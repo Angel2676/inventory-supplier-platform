f6f45e847cd7bb52509da7c998db0572a9ad35a6
Angelo Imperiale <angeloimperiale@MacBook-Air-di-Angelo.local>
2026-06-08 09:58:34 +0200

    Fix duplicate multer upload declaration

--- src/routes/marketplace.js
+++ src/routes/marketplace.js
@@ -2,7 +2,6 @@ const express = require("express");
 const multer = require("multer");
 const csv = require("csv-parser");
 const fs = require("fs");
-const upload = multer({ dest: "uploads/" });
 const pool = require("../db");
 
 const {
